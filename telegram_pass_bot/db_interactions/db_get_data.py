0b2afd2c7b1970ed99f61a43e9bed6c76a077649
Pineapple_Juice <st.okopnyi@gmail.com>
2023-03-04 19:52:03 +0700

    Refactor sign_in.py and db_interactions

--- telegram_pass_bot/db_interactions/db_get_data.py
+++ telegram_pass_bot/db_interactions/db_get_data.py
@@ -2,17 +2,22 @@ from sqlite3 import Cursor
 from typing import Any
 
 
-def get_hash_pass(cur: Cursor, telegram_id: int) -> tuple:
+def get_hash_pass(cur: Cursor, telegram_id: int) -> int | None:
     user = cur.execute("SELECT telegram_id, password_hash FROM users "
                        f"WHERE telegram_id = {telegram_id}; ")
-    return user.fetchone()
+    user = user.fetchone()
+    if user:
+        return user[1]
 
 
-def get_all_passwords(cur: Cursor, telegram_id: int) -> list[Any]:
+def get_all_passwords(cur: Cursor, telegram_id: int) -> list[tuple]:
     passwords = cur.execute("SELECT telegram_id, service, password "
                             "FROM passwords "
                             f"WHERE telegram_id = {telegram_id}; ")
-    return passwords.fetchall()
+    passwords = passwords.fetchall()
+    if passwords:
+        passwords = list(map(lambda x: (x[1], x[2]), passwords))
+    return passwords
 
 
 def get_service_passwords(cur: Cursor, telegram_id: int, service: str) -> list[Any]:
--- telegram_pass_bot/handlers/sign_in.py
+++ telegram_pass_bot/handlers/sign_in.py
@@ -2,10 +2,9 @@ from aiogram import Router
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
-from telegram_pass_bot.db_interactions import get_hash_pass, put_user_in_db, get_password_hash
-from telegram_pass_bot.db_interactions import con, cur
+from telegram_pass_bot.db_interactions import get_hash_pass, get_password_hash
+from telegram_pass_bot.db_interactions import cur
 
 router = Router()
 
@@ -15,39 +14,40 @@ class SignIn(StatesGroup):
     got_right_pass = State()
 
 
-def password_check(password: str, user_and_pass: tuple) -> bool:
+def password_check(password: str, hashed_password: str) -> bool:
     password = get_password_hash(password)
-    hashed_pass_from_db = user_and_pass[1]
+    hashed_pass_from_db = hashed_password
     return password == hashed_pass_from_db
 
 
 @router.message(Command("sign_in"))
 async def pass_await(message: Message, state: FSMContext):
-    user_and_pass = get_hash_pass(cur, message.from_user.id)
-    if not user_and_pass:
+    password = get_hash_pass(cur, message.from_user.id)
+    if not password:
         await message.answer("Вы еще не зарегистрированы в боте. "
                              "Для регистрации введите комманду /registration.")
         return
     await message.answer("Введите пароль:")
     await state.set_state(SignIn.awaiting_password)
-    await state.update_data(tries=0)
+    await state.update_data(tries=1)
 
 
 @router.message(SignIn.awaiting_password)
 async def bot_password_check(message: Message, state: FSMContext):
-    user_and_pass = get_hash_pass(cur, message.from_user.id)
+    password = get_hash_pass(cur, message.from_user.id)
     data = await state.get_data()
     tries_num = data["tries"]
+
+    if password_check(message.text, password):
+        await message.answer("Авторизация прошла успешно.")
+        await state.set_state(SignIn.got_right_pass)
+        return
+
+    await message.answer("Неверный пароль")
 
     if tries_num >= 3:
         await message.answer("Неудачная авторизация")
         await state.clear()
         return
 
-    if password_check(message.text, user_and_pass):
-        await message.answer("Авторизация прошла успешно.")
-        await state.set_state(SignIn.got_right_pass)
-        return None
-
-    await message.answer("Неверный пароль")
     await state.update_data(tries=(tries_num + 1))
